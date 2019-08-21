88a22b2d2550665139874fa6fa4adc253bfec319
vicMck <sorcervic@gmail.com>
2019-08-21 10:02:20 +0200

    Juego finalizado

--- src/AdivinaElNumero.java
+++ src/AdivinaElNumero.java
@@ -4,17 +4,38 @@ public class AdivinaElNumero {
 
     private Scanner entrada = new Scanner(System.in);
     private boolean juegoActivo = true;
+
     public void jugar() {
         System.out.println("Hola, ¿cuál es tu nombre?");
         String nombreJugador = entrada.nextLine();
         System.out.printf("Bienvenido %s, vamos a comenzar\n", nombreJugador);
 
-        while(juegoActivo) {
+        while (juegoActivo) {
+            int intentos = 0;
             int min = 0;
-            int max = 100;
+            int max = 20;
             int numeroJuego = (int) (Math.random() * ((max - min) + 1));
 
-            System.out.println(numeroJuego);
+
+            System.out.printf("%s he escogido un número entre %d y %d, adivínalo\n", nombreJugador, min, max);
+            int numeroJugador;
+
+
+            do {
+                System.out.println("Escoge un número");
+                numeroJugador = entrada.nextInt();
+
+                if (numeroJuego < numeroJugador) {
+                    System.out.println("Muy alto, adivina otra vez");
+                } else if (numeroJuego > numeroJugador) {
+                    System.out.println("Muy bajo, adivina otra vez");
+                }
+                intentos++;
+
+            } while (numeroJuego != numeroJugador);
+
+            System.out.printf("Has ganado, intentos %d", intentos);
+
             juegoActivo = false;
         }
 
